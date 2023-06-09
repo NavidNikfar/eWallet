9749e8643105d875f800b7500c57a9f39e83b7f6
JavaBaz <Bazgir.civil@gmail.com>
2023-06-09 13:15:16 +0330

    Add connection to UserRepImpl.java class.

--- src/main/java/repository/impl/UserRepImpl.java
+++ src/main/java/repository/impl/UserRepImpl.java
@@ -2,5 +2,16 @@ package repository.impl;
 
 import repository.UserRepo;
 
+import java.sql.Connection;
+
+
 public class UserRepImpl implements UserRepo {
+
+    Connection connection;
+
+    public UserRepImpl(Connection connection) {
+        this.connection = connection;
+    }
+
+
 }
